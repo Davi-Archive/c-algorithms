082818da3846c5f390c912c8a77f16bef69f43bc
d1av <davi4alves@gmail.com>
2022-11-15 16:34:57 -0300

    feat: new algorithm
    this new algorithmn has the function to show
    and give an idea about how the classes are structured
    how access works in C#.

--- 003 - heranca/003 - heranca/Mamifero.cs
+++ 003 - heranca/003 - heranca/Mamifero.cs
@@ -2,8 +2,15 @@
 {
     public class Mamifero : Animal
     {
+
+        public Mamifero(string nome, string especie)
+        {
+            Nome = nome;
+            Especie = especie;
+        }
         public void Mamar()
         {
+            Respirar();
             Console.WriteLine($"Animal {Nome} da {Especie} mamando.");
         }
     }
--- 003 - heranca/003 - heranca/Program.cs
+++ 003 - heranca/003 - heranca/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _003___heranca;
+string nome;
+string especie;
+
+Console.WriteLine("Informe o nome do aniumal:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a espécie: ");
+especie = Console.ReadLine();
+
+
+Mamifero mamifero = new Mamifero(nome, especie);
+
+mamifero.Mamar();

45f752ab133208f68c062420f0296c0ec22aec31
d1av <davi4alves@gmail.com>
2022-11-15 16:48:14 -0300

    feat: add new inheritance
    New inheritance classes with the
    more examples

--- 003 - heranca/003 - heranca/Program.cs
+++ 003 - heranca/003 - heranca/Program.cs
@@ -12,3 +12,25 @@ especie = Console.ReadLine();
 Mamifero mamifero = new Mamifero(nome, especie);
 
 mamifero.Mamar();
+
+Console.WriteLine("Informe o nome do réptil");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a espécie: ");
+especie = Console.ReadLine();
+
+Console.WriteLine("Informe se o réptil possui veneno: (Sim/Nao) ");
+string respostaveneno = Console.ReadLine();
+
+Reptil reptil = new Reptil(nome, especie);
+if (respostaveneno == "Sim")
+{
+    reptil.PossuiVeneno = true;
+}
+else
+{
+    reptil.PossuiVeneno = false;
+}
+
+reptil.BotarOvo();
+
--- 003 - heranca/003 - heranca/Reptil.cs
+++ 003 - heranca/003 - heranca/Reptil.cs
@@ -3,10 +3,22 @@
     public class Reptil : Animal
     {
         public bool PossuiVeneno { get; set; }
+        public Reptil(string nome, string especie)
+        {
+            Nome = nome;
+            Especie = especie;
 
+        }
         public void BotarOvo()
         {
-            Console.WriteLine($"Animal {Nome} da espécie {Especie} está botando ovo!");
+            if (PossuiVeneno)
+            {
+                Console.WriteLine($"Animal {Nome} da espécie {Especie} está botando ovo e é venenoso!");
+            }
+            else
+            {
+                Console.WriteLine($"Animal {Nome} da espécie {Especie} está botando ovo e NÂO é venenoso!");
+            }
         }
 
     }

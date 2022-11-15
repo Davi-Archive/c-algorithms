e908fbcda32842abcf095b28c65a7f62e4549885
d1av <davi4alves@gmail.com>
2022-11-15 13:23:39 -0300

    feat: new algorithm

--- c-algorithms/Program.cs
+++ c-algorithms/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string olaMundo = "Olá mundo, este é meu primeiro programa c# ";
+string nomeCompleto;
+int idade;
+
+Console.WriteLine("Qual seu nome completo?");
+
+nomeCompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade == 34 || nomeCompleto == "Davi")
+// if (idade == 34 && nomeCompleto == "Davi")
+{
+    Console.WriteLine(olaMundo + "meu nome é " + nomeCompleto + " minha idade é " + idade);
+}
+else
+{
+    Console.WriteLine("Voce errou a idade ou o nome");
+}

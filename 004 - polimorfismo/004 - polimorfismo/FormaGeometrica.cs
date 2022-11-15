c3da60fcec49a6d3d3165280f1253a67b68e7316
d1av <davi4alves@gmail.com>
2022-11-15 17:09:53 -0300

    feat: new polymorphism

---
+++ 004 - polimorfismo/004 - polimorfismo/FormaGeometrica.cs
@@ -0,0 +1,26 @@
+﻿namespace _004___polimorfismo
+{
+    public class FormaGeometrica
+    {
+
+        public virtual void Desenhar()
+        {
+
+        }
+    }
+
+    public class Circulo : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um circulo");
+        }
+    }
+    public class Quadrado : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um quadrado");
+        }
+    }
+}
---
+++ 004 - polimorfismo/004 - polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using _004___polimorfismo;
+
+List<FormaGeometrica> formas = new List<FormaGeometrica> {
+   new Circulo(),
+   new Quadrado(),
+   new Quadrado()
+};
+
+foreach (FormaGeometrica forma in formas)
+{
+    forma.Desenhar();
+}

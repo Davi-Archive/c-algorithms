adf2e58a731497d52d74de79f49987ee310d9647
d1av <davi4alves@gmail.com>
2022-11-15 14:06:01 -0300

    feat: add new message

--- c-algorithms/Program.cs
+++ c-algorithms/Program.cs
@@ -1,12 +1,16 @@
 ﻿
 
 string mensagem;
+string continuar;
 
-try
+do
 {
-    string produtoSelecionado;
 
-    List<string> listaMercado = new List<string>()
+    try
+    {
+        string produtoSelecionado;
+
+        List<string> listaMercado = new List<string>()
     {
         "Tomate",
         "Cebola",
@@ -20,17 +24,34 @@ try
         "Mel"
     };
 
-    Console.WriteLine("Digite o produto que gostaria de comprar");
+        Console.WriteLine("Digite o produto que gostaria de comprar?");
 
-    produtoSelecionado = Console.ReadLine();
+        produtoSelecionado = Console.ReadLine();
 
-    Console.WriteLine();
+        bool validaProduto = listaMercado.Where(l => l.Equals(produtoSelecionado)).Any();
 
-}
-catch (Exception ex)
-{
-    mensagem = "Aconteceu o seguint problema: " + ex.Message;
-}
-
-Console.WriteLine("Digite o produto que gostaria de comprar: ");
-produtoSelecionado = Console.ReadLine();
+        if (validaProduto)
+        {
+            Console.WriteLine("Pode comprar o produto " + produtoSelecionado + " pois ele está disponível");
+        }
+        else
+        {
+            Console.WriteLine("O produto " + produtoSelecionado + " não está a venda.");
+        }
+        Console.WriteLine(" ");
+        foreach (string produto in listaMercado.OrderBy(lista => lista))
+        {
+            Console.WriteLine(produto);
+        }
+    }
+    catch (Exception ex)
+    {
+        mensagem = "Aconteceu o seguint problema: " + ex.Message;
+    }
+    finally
+    {
+        Console.WriteLine("Obrigado por utilizar o sistema do mercado.");
+        Console.WriteLine("Deseja continuar? digite para:  [ Sim = s / Não = n ]");
+        continuar = Console.ReadLine().ToString();
+    }
+} while (continuar != "n");
